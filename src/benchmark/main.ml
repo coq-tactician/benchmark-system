dc40eef29fb48bc6e236d8957e5955525f00a300
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2022-05-17 16:16:36 +0200

    Exclude .vo.bench from rsync

--- src/benchmark/main.ml
+++ src/benchmark/main.ml
@@ -937,7 +937,8 @@ let main
              ; "opam-root/bench/.opam-switch/build/dune.*"
              ; "opam-root/bench/.opam-switch/build/dose3.*"
              ; "opam-root/download-cache"
-             ; "opam-root/repo"] in
+             ; "opam-root/repo"
+             ; "*.vo.bench"] in
            let exclude = List.concat @@ List.map ~f:(fun d -> ["--exclude"; d]) exclude in
            let args = [ target; "rsync"; "-qa" ] @ exclude @ [ !data_host^":"^scratch^"/"; scratch^"/" ] in
            Process.run
